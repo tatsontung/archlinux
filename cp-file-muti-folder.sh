874f14a33b7292f911413347cf6f2c38e5a18a82
Nguyen Tat Son Tung <tatsontung@hotmail.com>
2017-01-01 08:45:37 +0100

    Clean Up file and add Script

--- cp-file-muti-folder.sh
+++ cp-file-muti-folder.sh
@@ -1,2 +1,7 @@
 for dir in *; do [ -d "$dir" ] && cp /path/file.txt "$dir" ; done
 for dir in *; do [ -d "$dir" ] && cp Darkdreamer/layout-local.txt "$dir" ; done
+for dir in *; do [ -d "$dir" ] && mv "$dir"/*.mp4 . ; done
+for dir in *; do [ -d "$dir" ] && mv "$dir"/*.wmv . ; done
+for dir in *; do [ -d "$dir" ] && rm -rf "$dir"/* ; done
+
+for f in *.mp4 *.avi ; do d="${f%-*}" && mkdir "$d" && mv "$f" "$d/" ; done
