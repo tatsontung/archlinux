756368537548049947a59231896e40416ef6560e
Nguyen Tat Son Tung <tatsontung.nguyen@gmail.com>
2017-07-31 01:29:20 +0200

    improve scripts

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,10 +3,6 @@
 # Fail immediately if any errors occur
 set -e
 
-echo "Caching password..."
-sudo -K
-sudo true;
-
 SETUP_TYPE=$1
 MY_DIR="$(dirname "$0")"
 clear
@@ -14,10 +10,12 @@ clear
 echo
 echo "Setting up a '$SETUP_TYPE' machine..."
 
-source ${MY_DIR}/${SETUP_TYPE/bootstrap.sh
+source ${MY_DIR}/${SETUP_TYPE}/bootstrap.sh
 
 # Note: Homebrew needs to be set up first
 source ${MY_DIR}/configuration-bash.sh
+cp ${MY_DIR}/zshrc ~/.zshrc
+chsh -s `which zsh`
 source ${MY_DIR}/git.sh
 source ${MY_DIR}/node.sh
 source ${MY_DIR}/java.sh
--- scripts/ubuntu/bootstrap.sh
+++ scripts/ubuntu/bootstrap.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Get root up in here
-sudo su
+# sudo su
 
 # Configuration for cntlm proxy parser
 # echo 'Acquire::http::Proxy "http://10.0.2.2:3128";' >> /etc/apt/apt.conf.d/99proxy
@@ -10,12 +10,12 @@ sudo su
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 
 # Visual Code Studio Repo
-curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
-mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
-sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+sudo curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
 
 # Update and begin installing some utility tools
-apt -y --force-yes update
-apt -y --force-yes install vim curl htop lsof golang perl ruby \
+sudo apt -y --force-yes update
+sudo apt -y --force-yes install vim curl htop lsof golang perl ruby \
 git memcached build-essential python mongodb nodejs code docker docker-compose vagrant virtualbox \
-screenfetch cowsay fortune-mod
+screenfetch cowsay fortune-mod zsh
