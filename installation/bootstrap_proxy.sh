c1c7df16966c223e1ae64507c3064ea0145ac011
Tat Son Tung NGUYEN <tatsontung.nguyen@gmail.com>
2017-07-30 23:54:10 +0200

    clean and optimize dist struct

--- installation/bootstrap_proxy.sh
+++
@@ -1,9 +0,0 @@
-#!bin/sh
-
-# Configuration for npm
-npm config set proxy http://127.0.0.1:3128
-npm config set https-proxy http://127.0.0.1:3128
-npm --https-proxy=http://127.0.0.1:3128 -g install karma
-
-# Configuration for homebrew
-ALL_PROXY=http://127.0.0.1:3128 brew install node
