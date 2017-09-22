74c07d1af81f279db8974e1394be5203a7516c85
Nguyen Tat Son Tung <tatsontung.nguyen@gmail.com>
2017-09-22 16:33:19 +0200

    Added npm cache folder and google chrome stable in ubuntu bootstrap

--- scripts/ubuntu/bootstrap.sh
+++ scripts/ubuntu/bootstrap.sh
@@ -12,6 +12,14 @@ sudo add-apt-repository ppa:webupd8team/atom
 sudo add-apt-repository ppa:ubuntu-desktop/ubuntu-make
 sudo add-apt-repository ppa:webupd8team/terminix
 
+echo "add typora"
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys BA300B7755AFCFAE
+sudo add-apt-repository 'deb http://typora.io linux/'
+
+echo "add google-chrome-stable"
+sudo add-apt-repository "deb https://dl.google.com/linux/chrome/deb/ stable main"
+sudo wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+
 echo "add node repos"
 sudo curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 
@@ -19,12 +27,9 @@ sudo apt -y --force-yes update
 sudo apt -y --force-yes upgrade
 sudo apt -y --force-yes install vim curl htop lsof git memcached build-essential \
 python mongodb docker docker-compose vagrant virtualbox terminix maven gradle nginx \
-screenfetch cowsay fortune-mod zsh atom nodejs \
+screenfetch cowsay fortune-mod zsh atom nodejs typora google-chrome-stable \
 openjdk-8-jdk ubuntu-make
 #umake android android-studio
 umake ide idea
 umake ide visual-studio-code --accept-license
 #umake go
-echo "Installing mvn"
-
-sudo apt install mvn
