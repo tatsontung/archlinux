5cbed2ef2532e7c5df9111836be93deff8c129b4
Tat Son Tung NGUYEN <tatsontung.nguyen@gmail.com>
2017-10-25 17:02:02 +0200

    Added atom plugins

--- scripts/configurations.sh
+++ scripts/configurations.sh
@@ -31,4 +31,6 @@ dconf load /com/gexperts/Tilix/ < files/terminix.dconf
 
 echo "Install atom package and extensions"
 apm install minimap file-icons autocomplete-modules color-picker \
-pigments git-plus platformio-ide-terminal atom-beautify atom-monokai monokai
+pigments git-plus platformio-ide-terminal atom-beautify atom-monokai monokai \
+atom-typescript ide-typescript language-typescript-grammars-only \
+linter linter-ui-default intentions busy-signal
