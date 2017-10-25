f96680f7a24947402e17001084787feff4c71ddd
Tat Son Tung NGUYEN <tatsontung.nguyen@gmail.com>
2017-10-25 16:18:43 +0200

    Redited the script for docker files when using behind cntlm proxy

--- concourse/docker-compose-keys.sh
+++ concourse/docker-compose-keys.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-
+# Configuration for the key using between the worker and the web
 mkdir -p /keys/web /keys/worker
 
 ssh-keygen -t rsa -f keys/web/tsa_host_key -N '' && \
@@ -8,3 +8,20 @@ ssh-keygen -t rsa -f keys/worker/worker_key -N '' && \
 cp keys/worker/worker_key.pub keys/web/authorized_worker_keys && \
 cp keys/web/tsa_host_key.pub keys/worker
 
+# Proxy configuration
+# Cntlm must be in mode gateway : Gateway yes
+sudo mkdir -p /etc/systemd/system/docker.service.d
+
+cat >/etc/systemd/system/docker.service.d/https-proxy.conf <<-EOF
+[Service]
+Environment="HTTP_PROXY=http://127.0.0.1:3128/" "NO_PROXY=localhost"
+EOF
+
+cat >/etc/systemd/system/docker.service.d/http-proxy.conf <<-EOF
+[Service]
+Environment="HTTP_PROXY=http://127.0.0.1:3128/" "NO_PROXY=localhost"
+EOF
+
+# Reload service configuration and show config
+sudo systemctl daemon-reloead
+sudo systemctl show --property=Environment docker
