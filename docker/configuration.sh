d27e7e9fe5ee345719abd9f1ce0cf71d464e33e2
Nguyen Tat Son Tung <tatsontung.nguyen@gmail.com>
2018-03-07 11:41:22 +0100

    Added timezone paris europe and fixed configuration

--- docker/configuration.sh
+++ docker/configuration.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo 'Setup docker daemon' 
-cat >/etc/docker/daemons.json <<-EOF
+cat >/etc/docker/daemon.json <<-EOF
 {
         "insecure-registries" : ["10.177.102.84:5000"]
 }
@@ -9,6 +9,8 @@ EOF
 
 echo 'Setup docker proxy'
 #/etc/systemd/system/docker.service.d/http-proxy.conf
+mkdir -p /etc/systemd/system/docker.service.d
+
 cat >/etc/systemd/system/docker.service.d/http-proxy.conf <<-EOF
 [Service]
 Environment="HTTP_PROXY=http://127.0.0.1:3128/" "NO_PROXY=localhost"
