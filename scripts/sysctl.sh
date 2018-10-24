d6bce2f3de757b5904de4998e215aa66accd9352
Tat Son Tung NGUYEN <tatsontung.nguyen@gmail.com>
2018-10-24 11:51:32 +0200

    Update sysctl script

--- scripts/sysctl.sh
+++ scripts/sysctl.sh
@@ -4,4 +4,5 @@ echo "setting sysctl to 9001!!!"
 
 # this number is for angular js watch files changes
 sudo sysctl fs.inotify.max_user_watches=524288
+sudo sysctl -p --system
 cp files/welcome.sh ~/.oh-my-zsh/custom/
