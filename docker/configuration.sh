873d73eb404dc20345ba63b92f77d5c95b62d3c3
Tat Son Tung NGUYEN <tatsontung.nguyen@gmail.com>
2019-02-20 13:52:21 +0100

    Simplified proxy conf for docker daemon

--- docker/configuration.sh
+++ docker/configuration.sh
@@ -3,7 +3,7 @@
 sudo echo 'Setup docker daemon' 
 cat >/etc/docker/daemon.json <<-EOF
 {
-        "insecure-registries" : ["10.177.102.84:5000"]
+        "insecure-registries" : ["10.0.0.50:5000"]
 }
 EOF
 
@@ -11,15 +11,9 @@ sudo echo 'Setup docker proxy'
 #/etc/systemd/system/docker.service.d/http-proxy.conf
 mkdir -p /etc/systemd/system/docker.service.d
 
-cat >/etc/systemd/system/docker.service.d/http-proxy.conf <<-EOF
+cat >/etc/systemd/system/docker.service.d/proxy.conf <<-EOF
 [Service]
-Environment="HTTP_PROXY=http://127.0.0.1:3128/" "NO_PROXY=localhost"
-EOF
-
-#/etc/systemd/system/docker.service.d/https-proxy.conf
-cat >/etc/systemd/system/docker.service.d/https-proxy.conf <<-EOF
-[Service]
-Environment="HTTP_PROXY=http://127.0.0.1:3128/" "NO_PROXY=localhost"
+Environment="HTTP_PROXY=http://127.0.0.1:3128/" "HTTPS_PROXY=http://127.0.0.1:3128/" "NO_PROXY=localhost"
 EOF
 
 sudo systemctl daemon-reload
