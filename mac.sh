5b8d38e2e18ed3a3a6bd7fb47afb72eb3d1967d7
Tat Son Tung NGUYEN <tatsontung.nguyen@gmail.com>
2019-11-26 00:54:24 +0100

    refactor startup scripts

--- mac.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-cd os/mac
-./setup.sh
--- ubuntu.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-export SCRIPTS_DIR="$(dirname "$0")/os"
-$SCRIPTS_DIR}/ubuntu/bootstrap.sh
-$SCRIPTS_DIR}/ubuntu/node.sh
-$SCRIPTS_DIR}/common-git.sh
-$SCRIPTS_DIR}/common-linux.sh
-#$SCRIPTS_DIR}/common-proxy.sh
