ecb88e0bb6d518917464d5104f452e4f35fedaa7
Tat Son Tung NGUYEN <tatsontung.nguyen@gmail.com>
2020-06-25 04:42:47 +0200

    updated bootstrap file for ubuntu

--- environments/linux/scripts/ubuntu/bootstrap.sh
+++ environments/linux/scripts/ubuntu/bootstrap.sh
@@ -5,11 +5,22 @@
 
 # Update all dependencies and install packages
 sudo add-apt-repository ppa:system76/pop
+sudo add-apt-repository ppa:lazygit-team/release
 
 sudo apt update -y 
 sudo apt upgrade -y 
-sudo apt install -y git build-essential vim wget curl htop lsof tilix tmux tty-clock neofetch cowsay fortune-mod figlet \
-pop-theme gnome-tweaks zsh taskwarrior copyq ranger mc cmus httpie tree \
-nginx chromium-browser python ruby memcached docker docker-compose \
+sudo apt install -y git build-essential vim neovim wget curl htop lsof tilix tmux tty-clock neofetch cowsay fortune-mod figlet \
+pop-theme gnome-tweaks zsh taskwarrior copyq ranger ueberzug ripgrep silversearcher-ag fd-find  mc cmus httpie tree xsel  \
+nginx chromium-browser python python3 python3-pip ruby memcached docker docker-compose snapd lazygit \
 openjdk-8-jdk openjdk-11-jdk
 
+# Python dependencies
+sudo pip3 install ueberzug
+sudo pip3 install pynvim
+sudp pip3 install neovim-remote
+
+# Install from snap package
+sudo snap install universal-ctags
+
+# Install lazy docker
+curl https://raw.githubusercontent.com/jesseduffield/lazydocker/master/scripts/install_update_linux.sh | bash
