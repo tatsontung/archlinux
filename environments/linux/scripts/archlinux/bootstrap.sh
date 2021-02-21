81763ad25659e3ff7ae3598106e90c99ece1271b
Tat Son Tung NGUYEN <tatsontung.nguyen@gmail.com>
2021-02-21 23:04:10 +0100

    kiss archlinux install

--- environments/linux/scripts/archlinux/bootstrap.sh
+++ environments/linux/scripts/archlinux/bootstrap.sh
@@ -1,16 +1,4 @@
 #!/bin/sh
 echo "Installation must have software for Archlinux workstation"
-sudo pacman -Sy git tilix screenfetch cowsay \
-cowfortune fortune-mod vim nodejs npm zsh htop lsof nginx \
-docker docker-compose docker-machine etcher taskwarrior copyq ranger mc
-
-echo "Archlinux custom packages for JAVA"
-yaourt -Syu --aur gradle maven jdk8-openjdk 
-
-echo "Installation IDE for Web JAVA and Android"
-yaourt -Syu --aur visual-studio-code-bin intellij-idea-ce
-
-echo "Installing gnome-keyring-dev"
-sudo apt-get install libgnome-keyring-dev
-cd /usr/share/doc/git/contrib/credential/gnome-keyring
-sudo make
+sudo yay -Syu git kitty neovim gvim zsh fish lsof nginx copyq ranger mc \
+tmux exfat-utils httpie tree xsel fzf fasd wget curl
